76e169dc8369dfe4654fe73ddf2879236c1cd320
Sideral TI <sideralti.app@gmail.com>
2025-01-19 04:43:19 -0300

    first commit

---
+++ src/main/java/com/sideralti/app/Software000Application.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Software000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Software000Application.class, args);
+	}
+
+}
